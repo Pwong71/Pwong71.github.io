bb83870ed9a7a2c411e6506025ea08051efa56cb
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-07-12 14:56:43 -0400

    Minor updates

--- scripts/reveal.js
+++ scripts/reveal.js
@@ -1,10 +1,9 @@
 ScrollReveal().reveal('.content', {
   reset: true,
   origin: 'bottom',
-  viewOffset: { top: 50, bottom: 500 },
+  viewOffset: { top: 100, bottom: 400 },
   beforeReveal: content => {
-    const sideItems = document.querySelectorAll('ol.sticky li');
-    sideItems.forEach(li => {
+    document.querySelectorAll('ol.sticky li').forEach(li => {
       const id = li.querySelector('h2').getAttribute('id');
       if (content.getAttribute('aria-labelledby') === id) li.classList.add('active');
       else li.removeAttribute('class');
