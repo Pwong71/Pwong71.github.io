744c87baa74aac093a86bf1a9ac432855d7be94a
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-07-13 00:21:06 -0400

    More bug fixes
    - Safari on iOS doesn't like animating flex-grow alone, it seems
    - Make header selection more optimized

--- scripts/reveal.js
+++ scripts/reveal.js
@@ -1,9 +1,16 @@
-const getHeader = content => document.querySelector(`h2#${content.getAttribute('aria-labelledby')}`).parentElement;
+// Too lazy to type .getAttribute 900 times
+const aria = el => el.getAttribute('aria-labelledby');
+
+// Run through and get all the headers beforehand
+const headers = [...document.querySelectorAll('.content')].map(content => {
+  const label = aria(content);
+  return { [ label ]: document.querySelector(`#${label}`).parentElement };
+}).reduce((acc, cur) => ({ ...acc, ...cur }), {});
 
 ScrollReveal().reveal('.content', {
   reset: true,
   origin: 'bottom',
   viewOffset: { top: 100, bottom: 400 },
-  beforeReveal: content => getHeader(content).classList.add('active'),
-  afterReset: content => getHeader(content).classList.remove('active')
+  beforeReveal: content => headers[aria(content)].classList.add('active'),
+  afterReset: content => headers[aria(content)].classList.remove('active')
 });
