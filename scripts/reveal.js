ef9de49ef2fabb982721f86fc35613db98c9e19a
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-07-12 22:42:06 -0400

    Minor bug fixes
    - Fix last image in main carousel's title not showing up
      (cut off by keen-slider overflow)
    - Improve heading flex-grow on content pages
    - Minor spacing change for titles

--- scripts/reveal.js
+++ scripts/reveal.js
@@ -1,12 +1,9 @@
+const getHeader = content => document.querySelector(`h2#${content.getAttribute('aria-labelledby')}`).parentElement;
+
 ScrollReveal().reveal('.content', {
   reset: true,
   origin: 'bottom',
   viewOffset: { top: 100, bottom: 400 },
-  beforeReveal: content => {
-    document.querySelectorAll('ol.sticky li').forEach(li => {
-      const id = li.querySelector('h2').getAttribute('id');
-      if (content.getAttribute('aria-labelledby') === id) li.classList.add('active');
-      else li.removeAttribute('class');
-    });
-  }
+  beforeReveal: content => getHeader(content).classList.add('active'),
+  afterReset: content => getHeader(content).classList.remove('active')
 });
